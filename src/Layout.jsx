b5ea087624c7417f63b6619ed448eb2af929e2bd
AsimSiddiqui793264 <asimsiddiqui939@gmail.com>
2026-09-07 20:22:18 -0700

    fix: Correct component import paths in Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import { useLocation } from "react-router-dom";
 
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const Layout = ({ children }) => {
   const location = useLocation();
