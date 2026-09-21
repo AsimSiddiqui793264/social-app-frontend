e329f8cd15e3f9eb6decb55630e667ea5e4c9a12
AsimSiddiqui793264 <asimsiddiqui939@gmail.com>
2026-09-21 15:25:19 -0700

    refactor: reorganize imports and enhance rendering structure in main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,60 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signup from './Pages/Signup.jsx';
-import Login from './Pages/Login.jsx';
-import Layout from './Layout.jsx';
-import Home from './Pages/Home.jsx';
-import CreatePost from './components/CreatePost.jsx';
-import { ToastContainer } from "react-toastify";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { store } from "./app/store";
+
+import App from "./App.jsx";
+import Layout from "./Layout.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Login from "./Pages/Login.jsx";
+import Home from "./Pages/Home.jsx";
+import CreatePost from "./components/CreatePost.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-   <BrowserRouter>
-  <Layout>
-     <Routes>
-    <Route path='/' element={<App />} />
-    <Route path='/signup' element={<Signup />} />
-    <Route path='/login' element={<Login />} />
-    <Route path='/home' element={<Home />} />
-    <Route path='/create-post' element={<CreatePost />} />
-    {/* <Route path='/home' element={<Home />} /> */}
-    {/* <Route path='/home' element={<Home />} /> */}
-   </Routes>
-  </Layout>
-   </BrowserRouter>
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<App />} />
 
-  <ToastContainer
-            position="top-right"
-            autoClose={3000}
-        />
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
 
-  </StrictMode>,
-)
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+
+            <Route
+              path="/home"
+              element={<Home />}
+            />
+
+            <Route
+              path="/create-post"
+              element={<CreatePost />}
+            />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+      />
+    </Provider>
+  </React.StrictMode>
+);
